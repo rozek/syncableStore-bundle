f4c36866badafb38487c20b1969e79dea8303797
Andreas Rozek <A.Rozek@gmx.de>
2024-09-03 16:48:12 +0200

    Create syncableStore-bundle.ts

---
+++ src/syncableStore-bundle.ts
@@ -0,0 +1,33 @@
+export * from 'yjs'
+
+// @ts-ignore 
+import {
+  syncableStore, SyncedDoc,SyncedArray,SyncedMap,SyncedXml,SyncedText, Box,boxed,
+  observeDeep, areSame, 
+  Y, getYjsDoc,getYjsValue, transact
+} from "syncableStore"
+// @ts-ignore 
+import { syncedStore } from "@syncedstore/core"
+// @ts-ignore 
+import { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence } from 'y-indexeddb'
+// @ts-ignore
+import { WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync } from 'y-websocket'
+// @ts-ignore
+import { WebrtcConn, Room, SignalingConn, WebrtcProvider } from 'y-webrtc'
+// @ts-ignore
+import { YKeyValue } from 'y-utility/y-keyvalue'
+// @ts-ignore
+import { LWWMap } from 'y-lwwmap'
+// @ts-ignore
+import { Observable } from 'lib0/observable'
+
+export {
+  PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence,
+  WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync,
+  WebrtcConn, Room, SignalingConn, WebrtcProvider,
+  YKeyValue, LWWMap,
+  Observable,
+  syncableStore, SyncedDoc,SyncedArray,SyncedMap,SyncedXml,SyncedText, Box,boxed,
+  observeDeep, areSame, 
+  Y, getYjsDoc,getYjsValue, transact
+}
